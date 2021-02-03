623821444b7062f0b792e2ecb5cdc0302baa1ee1
jcapt <adam.ghtr@gmail.com>
2021-02-03 19:49:49 +0100

    Add file registry. Allow to delete files from registry.

--- endpoint.py
+++ endpoint.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from uuid import uuid4
+from registry import FileRegistry
 
 UPLOAD_FOLDER = './tmpfiles'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 
+filereg = FileRegistry()
+
 @app.route('/<entity_id>', methods=['GET', 'POST'])
 def upload_file(entity_id):
     if request.method == 'POST':
@@ -17,15 +20,22 @@ def upload_file(entity_id):
             flash('No file part')
             return "", 400
         file = request.files['file']
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'] + "/" + entity_id)
 
-        if not os.path.isdir("./" + entity_id):
-            os.mkdir(app.config['UPLOAD_FOLDER'] + "/" + entity_id)
+        if not os.path.isdir(filepath):
+            os.mkdir(filepath)
 
         if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'] + "/" + entity_id, gen_unique_filename(filename)))
+            filename = gen_unique_filename(filename)
+            file.save(filepath + filename)
+            filereg.register(entity_id, filename)
             return "", 201
 
+@app.route("/<entity_id>/<filename>", methods=["DELETE"])
+def delete_file(entity_id, filename):
+    filereg.deregister(entity_id, filename)
+    return "", 204
 
 def gen_unique_filename(filename):
     return filename + "." + str(uuid4())
--- registry.py
+++ registry.py
@@ -1,10 +1,17 @@
+import requests
+
+REGISTRY_VOLUME_URL = "http://localhost:3001"
+
 class FileRegistry():
-    def register(path, filename, metadata={}):
-        pass
+    def __init__(self):
+        self.session = requests.Session()
 
-    def deregister(path, filename, metadata={}):
-        pass
+    def register(self, entity_id, filename, metadata={}):
+        return self.session.put(REGISTRY_VOLUME_URL + "/" + entity_id + "/" + filename, data=metadata)
 
-    def get_file_metadata(path, filename):
-        pass
+    def deregister(self, entity_id, filename):
+        return self.session.delete(REGISTRY_VOLUME_URL +"/"+entity_id + "/" + filename) 
+
+    def get_file_metadata(self, path, filename):
+        return self.session.get(REGISTRY_VOLUME_URL + "/" + entity_id + "/" + filename)
 
